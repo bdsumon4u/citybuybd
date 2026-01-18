0c35d165376e7698969923b9557270f767aed18a
Sumon Ahmed <bdsumon4u@gmail.com>
2026-01-18 15:20:15 +0600

    feat(routes): apply middleware to employee and manager routes for authentication and role-based access control

--- routes/employee.php
+++ routes/employee.php
@@ -4,45 +4,45 @@ use App\Http\Controllers\Employee\OrderController as EmployeeOrderController;
 use App\Http\Controllers\Employee\PagesController as EmployeePagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'employee', 'middleware' => ['auth', 'employee']], function (): void {
+Route::group(['prefix' => 'employee'], function (): void {
     // dashboard
-    Route::get('/dashboard', [EmployeePagesController::class, 'dashboard'])->name('employee.dashboard');
+    Route::get('/dashboard', [EmployeePagesController::class, 'dashboard'])->name('employee.dashboard')->middleware('auth', 'employee');
 
-    Route::get('reset', fn () => view('employee.pages.reset'))->name('employee.reset');
-    Route::post('r_store', [EmployeePagesController::class, 'r_store'])->name('employee.r_store');
+    Route::get('reset', fn () => view('employee.pages.reset'))->name('employee.reset')->middleware('auth', 'employee');
+    Route::post('r_store', [EmployeePagesController::class, 'r_store'])->name('employee.r_store')->middleware('auth', 'employee');
 
     // Order Management Route
     Route::group(['prefix' => '/order-management'], function (): void {
-        Route::get('/manage-old', [EmployeeOrderController::class, 'index'])->name('employee.order.manage');
-        Route::get('/manage/{status}', [EmployeeOrderController::class, 'management'])->name('employee.order.management');
+        Route::get('/manage-old', [EmployeeOrderController::class, 'index'])->name('employee.order.manage')->middleware('auth', 'employee');
+        Route::get('/manage/{status}', [EmployeeOrderController::class, 'management'])->name('employee.order.management')->middleware('auth', 'employee');
 
         // new update
-        Route::get('/manage', [EmployeeOrderController::class, 'newIndex'])->name('employee.order.newmanage');
-        Route::get('/filter-data', [EmployeeOrderController::class, 'FilterData'])->name('employee.filter-data');
-        Route::get('/new-manage-action', [EmployeeOrderController::class, 'newIndexAction'])->name('employee.new-manage-action');
-        Route::get('emp-total-order-list', [EmployeeOrderController::class, 'total_order_list'])->name('emp_total_order_list');
-        Route::get('/parcel-handover', [EmployeeOrderController::class, 'parcelHandover'])->name('employee.order.parcelHandover');
-        Route::post('/scan-parcel-handover', [EmployeeOrderController::class, 'scanParcelHandover'])->name('employee.order.scanParcelHandover');
-        Route::get('/return-received', [EmployeeOrderController::class, 'returnReceived'])->name('employee.order.returnReceived');
-        Route::post('/scan-return-received', [EmployeeOrderController::class, 'scanReturnReceived'])->name('employee.order.scanReturnReceived');
-        Route::get('/get-scanned-orders', [EmployeeOrderController::class, 'getScannedOrders'])->name('employee.order.getScannedOrders');
-        Route::get('/print-scanned-orders', [EmployeeOrderController::class, 'printScannedOrders'])->name('employee.order.printScannedOrders');
-        Route::post('/delete-scanned-order', [EmployeeOrderController::class, 'deleteScannedOrder'])->name('employee.order.deleteScannedOrder');
-        Route::get('order-details/{id}', [EmployeeOrderController::class, 'show'])->name('employee.order.details');
-        Route::get('create', [EmployeeOrderController::class, 'create'])->name('employee.order.create');
-        Route::post('store', [EmployeeOrderController::class, 'store'])->name('employee.order.store');
-        Route::get('/edit/{id}', [EmployeeOrderController::class, 'edit'])->name('employee.order.edit');
-        Route::post('/update/{id}', [EmployeeOrderController::class, 'update'])->name('employee.order.update');
+        Route::get('/manage', [EmployeeOrderController::class, 'newIndex'])->name('employee.order.newmanage')->middleware('auth', 'employee');
+        Route::get('/filter-data', [EmployeeOrderController::class, 'FilterData'])->name('employee.filter-data')->middleware('auth', 'employee');
+        Route::get('/new-manage-action', [EmployeeOrderController::class, 'newIndexAction'])->name('employee.new-manage-action')->middleware('auth', 'employee');
+        Route::get('emp-total-order-list', [EmployeeOrderController::class, 'total_order_list'])->name('emp_total_order_list')->middleware('auth', 'employee');
+        Route::get('/parcel-handover', [EmployeeOrderController::class, 'parcelHandover'])->name('employee.order.parcelHandover')->middleware('auth');
+        Route::post('/scan-parcel-handover', [EmployeeOrderController::class, 'scanParcelHandover'])->name('employee.order.scanParcelHandover')->middleware('auth');
+        Route::get('/return-received', [EmployeeOrderController::class, 'returnReceived'])->name('employee.order.returnReceived')->middleware('auth');
+        Route::post('/scan-return-received', [EmployeeOrderController::class, 'scanReturnReceived'])->name('employee.order.scanReturnReceived')->middleware('auth');
+        Route::get('/get-scanned-orders', [EmployeeOrderController::class, 'getScannedOrders'])->name('employee.order.getScannedOrders')->middleware('auth');
+        Route::get('/print-scanned-orders', [EmployeeOrderController::class, 'printScannedOrders'])->name('employee.order.printScannedOrders')->middleware('auth');
+        Route::post('/delete-scanned-order', [EmployeeOrderController::class, 'deleteScannedOrder'])->name('employee.order.deleteScannedOrder')->middleware('auth');
+        Route::get('order-details/{id}', [EmployeeOrderController::class, 'show'])->name('employee.order.details')->middleware('auth', 'employee');
+        Route::get('create', [EmployeeOrderController::class, 'create'])->name('employee.order.create')->middleware('auth', 'employee');
+        Route::post('store', [EmployeeOrderController::class, 'store'])->name('employee.order.store')->middleware('auth', 'employee');
+        Route::get('/edit/{id}', [EmployeeOrderController::class, 'edit'])->name('employee.order.edit')->middleware('auth', 'employee');
+        Route::post('/update/{id}', [EmployeeOrderController::class, 'update'])->name('employee.order.update')->middleware('auth', 'employee');
 
         // order status change
-        Route::get('/order/{status}/{id}', [EmployeeOrderController::class, 'statusChange'])->name('employee.order.statusChange');
+        Route::get('/order/{status}/{id}', [EmployeeOrderController::class, 'statusChange'])->name('employee.order.statusChange')->middleware('auth', 'employee');
 
         // order export & optional
-        Route::post('/update_s/{id}', [EmployeeOrderController::class, 'update_s'])->name('employee.order.update_s');
-        Route::post('update_auto', [EmployeeOrderController::class, 'update_auto']);
-        Route::get('order-export', [EmployeeOrderController::class, 'orderexport'])->name('employee.order.export');
-        Route::get('/searchInput', [EmployeeOrderController::class, 'search_order_input'])->name('order.search.input.employee');
+        Route::post('/update_s/{id}', [EmployeeOrderController::class, 'update_s'])->name('employee.order.update_s')->middleware('auth', 'employee');
+        Route::post('update_auto', [EmployeeOrderController::class, 'update_auto'])->middleware('auth', 'employee');
+        Route::get('order-export', [EmployeeOrderController::class, 'orderexport'])->name('employee.order.export')->middleware('auth', 'employee');
+        Route::get('/searchInput', [EmployeeOrderController::class, 'search_order_input'])->name('order.search.input.employee')->middleware('auth', 'employee');
 
-        Route::post('noted_edit/{id}', [EmployeeOrderController::class, 'noted_edit'])->name('employee.order.noted_edit');
+        Route::post('noted_edit/{id}', [EmployeeOrderController::class, 'noted_edit'])->name('employee.order.noted_edit')->middleware('auth');
     });
 });
--- routes/manager.php
+++ routes/manager.php
@@ -9,104 +9,104 @@ use App\Http\Controllers\Manager\UserController as ManagerUserController;
 use App\Http\Controllers\Manager\ZoneController as ManagerZoneController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'manager', 'middleware' => ['auth', 'manager']], function (): void {
+Route::group(['prefix' => 'manager'], function (): void {
     // dashboard
-    Route::get('/dashboard', [ManagerPagesController::class, 'dashboard'])->name('manager.dashboard');
+    Route::get('/dashboard', [ManagerPagesController::class, 'dashboard'])->name('manager.dashboard')->middleware('auth', 'manager');
 
-    Route::get('managerreset', fn () => view('manager.pages.reset'))->name('manager.reset');
-    Route::post('r_store', [ManagerPagesController::class, 'r_store'])->name('manager.r_store');
+    Route::get('managerreset', fn () => view('manager.pages.reset'))->name('manager.reset')->middleware('auth', 'manager');
+    Route::post('r_store', [ManagerPagesController::class, 'r_store'])->name('manager.r_store')->middleware('auth', 'manager');
 
     // courier group
     Route::group(['prefix' => 'courier'], function (): void {
-        Route::get('/manage', [ManagerCourierController::class, 'index'])->name('manager.courier.manage');
-        Route::get('/create', [ManagerCourierController::class, 'create'])->name('manager.courier.create');
-        Route::post('/store', [ManagerCourierController::class, 'store'])->name('manager.courier.store');
-        Route::get('/edit/{id}', [ManagerCourierController::class, 'edit'])->name('manager.courier.edit');
-        Route::post('/update/{id}', [ManagerCourierController::class, 'update'])->name('manager.courier.update');
-        Route::post('/destroy/{id}', [ManagerCourierController::class, 'destroy'])->name('manager.courier.destroy');
+        Route::get('/manage', [ManagerCourierController::class, 'index'])->name('manager.courier.manage')->middleware('auth', 'manager');
+        Route::get('/create', [ManagerCourierController::class, 'create'])->name('manager.courier.create')->middleware('auth', 'manager');
+        Route::post('/store', [ManagerCourierController::class, 'store'])->name('manager.courier.store')->middleware('auth', 'manager');
+        Route::get('/edit/{id}', [ManagerCourierController::class, 'edit'])->name('manager.courier.edit')->middleware('auth', 'manager');
+        Route::post('/update/{id}', [ManagerCourierController::class, 'update'])->name('manager.courier.update')->middleware('auth', 'manager');
+        Route::post('/destroy/{id}', [ManagerCourierController::class, 'destroy'])->name('manager.courier.destroy')->middleware('auth', 'manager');
     });
 
     // City group
     Route::group(['prefix' => 'city'], function (): void {
-        Route::get('/manage', [ManagerCityController::class, 'index'])->name('manager.city.manage');
-        Route::get('/create', [ManagerCityController::class, 'create'])->name('manager.city.create');
-        Route::post('/store', [ManagerCityController::class, 'store'])->name('manager.city.store');
-        Route::get('/edit/{id}', [ManagerCityController::class, 'edit'])->name('manager.city.edit');
-        Route::post('/update/{id}', [ManagerCityController::class, 'update'])->name('manager.city.update');
-        Route::post('/destroy/{id}', [ManagerCityController::class, 'destroy'])->name('manager.city.destroy');
+        Route::get('/manage', [ManagerCityController::class, 'index'])->name('manager.city.manage')->middleware('auth', 'manager');
+        Route::get('/create', [ManagerCityController::class, 'create'])->name('manager.city.create')->middleware('auth', 'manager');
+        Route::post('/store', [ManagerCityController::class, 'store'])->name('manager.city.store')->middleware('auth', 'manager');
+        Route::get('/edit/{id}', [ManagerCityController::class, 'edit'])->name('manager.city.edit')->middleware('auth', 'manager');
+        Route::post('/update/{id}', [ManagerCityController::class, 'update'])->name('manager.city.update')->middleware('auth', 'manager');
+        Route::post('/destroy/{id}', [ManagerCityController::class, 'destroy'])->name('manager.city.destroy')->middleware('auth', 'manager');
     });
 
     // Zone group
     Route::group(['prefix' => 'zone'], function (): void {
-        Route::get('/manage', [ManagerZoneController::class, 'index'])->name('manager.zone.manage');
-        Route::get('/create', [ManagerZoneController::class, 'create'])->name('manager.zone.create');
-        Route::post('/store', [ManagerZoneController::class, 'store'])->name('manager.zone.store');
-        Route::get('/edit/{id}', [ManagerZoneController::class, 'edit'])->name('manager.zone.edit');
-        Route::post('/update/{id}', [ManagerZoneController::class, 'update'])->name('manager.zone.update');
-        Route::post('/destroy/{id}', [ManagerZoneController::class, 'destroy'])->name('manager.zone.destroy');
+        Route::get('/manage', [ManagerZoneController::class, 'index'])->name('manager.zone.manage')->middleware('auth', 'manager');
+        Route::get('/create', [ManagerZoneController::class, 'create'])->name('manager.zone.create')->middleware('auth', 'manager');
+        Route::post('/store', [ManagerZoneController::class, 'store'])->name('manager.zone.store')->middleware('auth', 'manager');
+        Route::get('/edit/{id}', [ManagerZoneController::class, 'edit'])->name('manager.zone.edit')->middleware('auth', 'manager');
+        Route::post('/update/{id}', [ManagerZoneController::class, 'update'])->name('manager.zone.update')->middleware('auth', 'manager');
+        Route::post('/destroy/{id}', [ManagerZoneController::class, 'destroy'])->name('manager.zone.destroy')->middleware('auth', 'manager');
     });
 
     // product group
     Route::group(['prefix' => '/product'], function (): void {
-        Route::get('/manage', [ManagerProductController::class, 'index'])->name('manager.product.manage');
-        Route::get('/create', [ManagerProductController::class, 'create'])->name('manager.product.create');
-        Route::post('/store', [ManagerProductController::class, 'store'])->name('manager.product.store');
-        Route::get('/edit/{id}', [ManagerProductController::class, 'edit'])->name('manager.product.edit');
-        Route::post('/update/{id}', [ManagerProductController::class, 'update'])->name('manager.product.update');
-        Route::get('/destroy/{id}', [ManagerProductController::class, 'destroy'])->name('manager.product.destroy');
-        Route::get('/assign_dlt/{id}', [ManagerProductController::class, 'assign_dlt'])->name('manager.assign_dlt');
-        Route::get('product-export', [ManagerProductController::class, 'exportIntoExcel'])->name('manager.product.export');
-        Route::post('/selected-products', [ManagerProductController::class, 'deleteChecketProducts'])->name('manager.deleteSelected');
-        Route::post('/p-selected-status', [ManagerProductController::class, 'p_selected_status'])->name('manager.p_selected_status');
+        Route::get('/manage', [ManagerProductController::class, 'index'])->name('manager.product.manage')->middleware('auth', 'manager');
+        Route::get('/create', [ManagerProductController::class, 'create'])->name('manager.product.create')->middleware('auth', 'manager');
+        Route::post('/store', [ManagerProductController::class, 'store'])->name('manager.product.store')->middleware('auth', 'manager');
+        Route::get('/edit/{id}', [ManagerProductController::class, 'edit'])->name('manager.product.edit')->middleware('auth', 'manager');
+        Route::post('/update/{id}', [ManagerProductController::class, 'update'])->name('manager.product.update')->middleware('auth', 'manager');
+        Route::get('/destroy/{id}', [ManagerProductController::class, 'destroy'])->name('manager.product.destroy')->middleware('auth', 'manager');
+        Route::get('/assign_dlt/{id}', [ManagerProductController::class, 'assign_dlt'])->name('manager.assign_dlt')->middleware('auth', 'manager');
+        Route::get('product-export', [ManagerProductController::class, 'exportIntoExcel'])->name('manager.product.export')->middleware('auth', 'manager');
+        Route::post('/selected-products', [ManagerProductController::class, 'deleteChecketProducts'])->name('manager.deleteSelected')->middleware('auth', 'manager');
+        Route::post('/p-selected-status', [ManagerProductController::class, 'p_selected_status'])->name('manager.p_selected_status')->middleware('auth', 'manager');
     });
 
     // user group
     Route::group(['prefix' => '/user'], function (): void {
-        Route::get('/manage', [ManagerUserController::class, 'index'])->name('manager.user.manage');
-        Route::get('/create', [ManagerUserController::class, 'create'])->name('manager.user.create');
-        Route::post('/store', [ManagerUserController::class, 'store'])->name('manager.user.store');
-        Route::get('/edit/{id}', [ManagerUserController::class, 'edit'])->name('manager.user.edit');
-        Route::post('/update/{id}', [ManagerUserController::class, 'update'])->name('manager.user.update');
-        Route::post('/destroy/{id}', [ManagerUserController::class, 'destroy'])->name('manager.user.destroy');
-        Route::get('user-export', [ManagerUserController::class, 'exportIntoExcel'])->name('manager.user.export');
-        Route::post('/selected-products', [ManagerUserController::class, 'deleteChecketProducts'])->name('manager.deleteSelectedU');
+        Route::get('/manage', [ManagerUserController::class, 'index'])->name('manager.user.manage')->middleware('auth', 'manager');
+        Route::get('/create', [ManagerUserController::class, 'create'])->name('manager.user.create')->middleware('auth', 'manager');
+        Route::post('/store', [ManagerUserController::class, 'store'])->name('manager.user.store')->middleware('auth', 'manager');
+        Route::get('/edit/{id}', [ManagerUserController::class, 'edit'])->name('manager.user.edit')->middleware('auth', 'manager');
+        Route::post('/update/{id}', [ManagerUserController::class, 'update'])->name('manager.user.update')->middleware('auth', 'manager');
+        Route::post('/destroy/{id}', [ManagerUserController::class, 'destroy'])->name('manager.user.destroy')->middleware('auth', 'manager');
+        Route::get('user-export', [ManagerUserController::class, 'exportIntoExcel'])->name('manager.user.export')->middleware('auth', 'manager');
+        Route::post('/selected-products', [ManagerUserController::class, 'deleteChecketProducts'])->name('manager.deleteSelectedU')->middleware('auth', 'manager');
     });
 
-    Route::get('stock', fn () => view('manager.pages.product.stock'))->name('manager.product.stock');
+    Route::get('stock', fn () => view('manager.pages.product.stock'))->name('manager.product.stock')->middleware('auth', 'manager');
 
     // Order Management Route
     Route::group(['prefix' => '/order-management'], function (): void {
-        Route::get('/manage', [ManagerOrderController::class, 'index'])->name('manager.order.manage');
-        Route::get('/manage/{status}', [ManagerOrderController::class, 'management'])->name('manager.order.management');
-        Route::get('order-details/{id}', [ManagerOrderController::class, 'show'])->name('manager.order.details');
-        Route::get('create', [ManagerOrderController::class, 'create'])->name('manager.order.create');
-        Route::post('store', [ManagerOrderController::class, 'store'])->name('manager.order.store');
-        Route::get('/edit/{id}', [ManagerOrderController::class, 'edit'])->name('manager.order.edit');
+        Route::get('/manage', [ManagerOrderController::class, 'index'])->name('manager.order.manage')->middleware('auth', 'manager');
+        Route::get('/manage/{status}', [ManagerOrderController::class, 'management'])->name('manager.order.management')->middleware('auth', 'manager');
+        Route::get('order-details/{id}', [ManagerOrderController::class, 'show'])->name('manager.order.details')->middleware('auth', 'manager');
+        Route::get('create', [ManagerOrderController::class, 'create'])->name('manager.order.create')->middleware('auth', 'manager');
+        Route::post('store', [ManagerOrderController::class, 'store'])->name('manager.order.store')->middleware('auth', 'manager');
+        Route::get('/edit/{id}', [ManagerOrderController::class, 'edit'])->name('manager.order.edit')->middleware('auth', 'manager');
 
         // new update
-        Route::get('/new-manage', [ManagerOrderController::class, 'newIndex'])->name('manager.order.newmanage');
-        Route::get('/filter-data', [ManagerOrderController::class, 'FilterData'])->name('manager.filter-data');
-        Route::get('/new-manage-action', [ManagerOrderController::class, 'newIndexAction'])->name('manager.new-manage-action');
-        Route::get('manager-total-order-list', [ManagerOrderController::class, 'total_order_list'])->name('manager_total_order_list');
-        Route::get('/parcel-handover', [ManagerOrderController::class, 'parcelHandover'])->name('manager.order.parcelHandover');
-        Route::post('/scan-parcel-handover', [ManagerOrderController::class, 'scanParcelHandover'])->name('manager.order.scanParcelHandover');
-        Route::get('/return-received', [ManagerOrderController::class, 'returnReceived'])->name('manager.order.returnReceived');
-        Route::post('/scan-return-received', [ManagerOrderController::class, 'scanReturnReceived'])->name('manager.order.scanReturnReceived');
-        Route::get('/get-scanned-orders', [ManagerOrderController::class, 'getScannedOrders'])->name('manager.order.getScannedOrders');
-        Route::get('/print-scanned-orders', [ManagerOrderController::class, 'printScannedOrders'])->name('manager.order.printScannedOrders');
-        Route::post('/delete-scanned-order', [ManagerOrderController::class, 'deleteScannedOrder'])->name('manager.order.deleteScannedOrder');
+        Route::get('/new-manage', [ManagerOrderController::class, 'newIndex'])->name('manager.order.newmanage')->middleware('auth', 'manager');
+        Route::get('/filter-data', [ManagerOrderController::class, 'FilterData'])->name('manager.filter-data')->middleware('auth', 'manager');
+        Route::get('/new-manage-action', [ManagerOrderController::class, 'newIndexAction'])->name('manager.new-manage-action')->middleware('auth', 'manager');
+        Route::get('manager-total-order-list', [ManagerOrderController::class, 'total_order_list'])->name('manager_total_order_list')->middleware('auth', 'manager');
+        Route::get('/parcel-handover', [ManagerOrderController::class, 'parcelHandover'])->name('manager.order.parcelHandover')->middleware('auth');
+        Route::post('/scan-parcel-handover', [ManagerOrderController::class, 'scanParcelHandover'])->name('manager.order.scanParcelHandover')->middleware('auth');
+        Route::get('/return-received', [ManagerOrderController::class, 'returnReceived'])->name('manager.order.returnReceived')->middleware('auth');
+        Route::post('/scan-return-received', [ManagerOrderController::class, 'scanReturnReceived'])->name('manager.order.scanReturnReceived')->middleware('auth');
+        Route::get('/get-scanned-orders', [ManagerOrderController::class, 'getScannedOrders'])->name('manager.order.getScannedOrders')->middleware('auth');
+        Route::get('/print-scanned-orders', [ManagerOrderController::class, 'printScannedOrders'])->name('manager.order.printScannedOrders')->middleware('auth');
+        Route::post('/delete-scanned-order', [ManagerOrderController::class, 'deleteScannedOrder'])->name('manager.order.deleteScannedOrder')->middleware('auth');
         // order status change
         Route::get('/order/{status}/{id}', [ManagerOrderController::class, 'statusChange'])->middleware('auth')->name('manager.order.statusChange');
 
-        Route::post('/update/{id}', [ManagerOrderController::class, 'update'])->name('manager.order.update');
-        Route::post('/update_s/{id}', [ManagerOrderController::class, 'update_s'])->name('manager.order.update_s');
-        Route::post('update_auto', [ManagerOrderController::class, 'update_auto']);
+        Route::post('/update/{id}', [ManagerOrderController::class, 'update'])->name('manager.order.update')->middleware('auth', 'manager');
+        Route::post('/update_s/{id}', [ManagerOrderController::class, 'update_s'])->name('manager.order.update_s')->middleware('auth', 'manager');
+        Route::post('update_auto', [ManagerOrderController::class, 'update_auto'])->middleware('auth', 'manager');
 
-        Route::post('/destroy/{id}', [ManagerOrderController::class, 'destroy'])->name('manager.order.destroy');
-        Route::get('order-export', [ManagerOrderController::class, 'orderexport'])->name('manager.order.export');
+        Route::post('/destroy/{id}', [ManagerOrderController::class, 'destroy'])->name('manager.order.destroy')->middleware('auth', 'manager');
+        Route::get('order-export', [ManagerOrderController::class, 'orderexport'])->name('manager.order.export')->middleware('auth', 'manager');
         Route::post('/selected-orders', [ManagerOrderController::class, 'deleteChecketorders'])->middleware('auth', 'manager')->name('manager.deleteChecketorders');
 
-        Route::post('/selected-status', [ManagerOrderController::class, 'selected_status'])->name('manager.selected_status');
+        Route::post('/selected-status', [ManagerOrderController::class, 'selected_status'])->name('manager.selected_status')->middleware('auth', 'manager');
 
         Route::get('/searchInput', [ManagerOrderController::class, 'search_order_input'])->middleware('auth', 'manager')->name('order.search.input.manager');
         Route::get('/paginate/{count}/{status}', [ManagerOrderController::class, 'paginate'])->middleware('auth', 'admin')->name('order.paginate.manager');
