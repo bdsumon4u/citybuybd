cbf2fc99322f2732df38175f74d1a934fdc5f781
Sumon Ahmed <bdsumon4u@gmail.com>
2026-04-14 07:04:40 +0600

    chore: update .env.example with timezone setting, add node engine requirement in package.json, and remove unused Pathao and RedX controllers

--- config/app.php
+++ config/app.php
@@ -81,7 +81,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE'),
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
