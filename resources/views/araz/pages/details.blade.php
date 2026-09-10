6c6cd65612674398e207191e9b2a31d2b520a38f
Sumon Ahmed <bdsumon4u@gmail.com>
2026-09-10 20:41:24 +0600

    fix: update old price label to পূর্ব মূল্য in product details view

--- resources/views/araz/pages/details.blade.php
+++ resources/views/araz/pages/details.blade.php
@@ -388,7 +388,7 @@
                             মূল্য: <span id="displayCurrentPrice">{{ number_format($currentPrice, 0) }}</span> টাকা
                         </span>
                         <span id="displayOldPriceWrapper" style="font-size: 15px; color: #64748b; font-weight: 500; {{ $oldPrice ? '' : 'display: none;' }} text-decoration: line-through;">
-                            মূল্য: <del id="displayOldPrice">{{ number_format($oldPrice ?? 0, 0) }}</del> টাকা
+                            পূর্ব মূল্য: <del id="displayOldPrice">{{ number_format($oldPrice ?? 0, 0) }}</del> টাকা
                         </span>
                     </div>
 
